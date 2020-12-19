546f1dcde5a032e98a971c1ad2f318d2a2de6657
Cancer <Cancer>
2020-12-20 00:42:20 +0400

    Added date into the file CN_debug_{date}.txt

--- verify-cn/Program.cs
+++ verify-cn/Program.cs
@@ -7,12 +7,14 @@ namespace verify_cn
     {
         static void LogWrite(string UidFile, string Depth, string UID, string VpnAction)
         {
-            string PathLogFolder = @"..\log";
+            string PathLogFolder = @"..\log\";
             if (!Directory.Exists(PathLogFolder))
             {
                 Directory.CreateDirectory(PathLogFolder);
             }
-            string PathLogFile = @"..\log\CN_debug.txt";
+            string LogFile = $"CN_debug_{DateTime.Now:yyyy.MM.dd}.txt";
+            string PathLogFile = PathLogFolder + LogFile;
+            //string PathLogFile = @"..\log\CN_debug.txt";
             DateTime now = DateTime.Now;
             string LogText = now.ToString() + ": " + $"FileName = {UidFile}, Depth = {Depth}, UID = {UID}, Action = {VpnAction}" + "\n";
             System.IO.File.AppendAllText(PathLogFile, LogText);
@@ -25,18 +27,19 @@ namespace verify_cn
             string UidInput = args[2];  // CN
             UidInput = UidInput.Substring(3); // cut 3 first symbols, because ovpn sending CN=CLIENTNAME
 
-            if (Depth == "1") // if depth eq 0 need to pass it, because this step is checking CA
+            // if depth eq 1 need to pass it, because this step is checking CA
+            if (Depth == "1") 
             {
                 return 0;
             }
 
             if (Depth == "0")
             {
-                                                
+                
                 var text = File.ReadAllLines(UidFile);
-                foreach (var uids in text)
+                foreach (var UidList in text)
                 {
-                    if (uids == UidInput)
+                    if (UidList == UidInput)
                     {
                         // Logging
                         string Allow = "Allow";
